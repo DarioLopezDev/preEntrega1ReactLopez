2fb4a455232c86ed30f840da0ca4f14a7901974f
Dario Lopez <dariolopezdeveloper@gmail.com>
2023-10-29 07:29:52 -0300

    avanzo haciendo el NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import CartWidget from './CartWidget'
+import {Menu, MenuButton, MenuList,MenuItem,Flex,Box,Spacer } from '@chakra-ui/react'
+
+
+
+const NavBar = () => {
+  return (
+    <div>
+        <Flex>
+  <Box p='4'>
+        <h3>Brand</h3>
+  </Box>
+  <Spacer />
+
+        <Menu>
+  <MenuButton>
+    Categorias
+  </MenuButton>
+  <MenuList>
+    <MenuItem>A</MenuItem>
+    <MenuItem>B</MenuItem>
+    <MenuItem>C</MenuItem>
+    <MenuItem>D</MenuItem>
+    <MenuItem>E</MenuItem>
+  </MenuList>
+</Menu>
+  <Spacer />
+  <Box p='4'>
+        <CartWidget />
+  </Box>
+</Flex>
+
+    </div>
+  )
+}
+
+export default NavBar
--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,12 @@
-/* //Inicial
+//Inicial
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.jsx'
-import './index.css'
 
 //Inicial
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <ChakraProvider>
     <App />
-  </React.StrictMode>,
-) */
-
-//Para Usar ChakraUi
-import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import * as ReactDOM from 'react-dom/client'
-
-//Para Usar ChakraUi
-const rootElement = document.getElementById('root')
-ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
+  </ChakraProvider>,
 )
