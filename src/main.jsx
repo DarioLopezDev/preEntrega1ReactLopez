3dc96861ce3f7e8857f51c48d5d72c72b1696f8d
Dario Lopez <dariolopezdeveloper@gmail.com>
2023-10-28 23:25:51 -0300

    cargo chakraUi

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
+//Inicial
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//Inicial
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>,
+)
+
+//Para Usar ChakraUi
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import * as ReactDOM from 'react-dom/client'
+
+//Para Usar ChakraUi
+const rootElement = document.getElementById('root')
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
