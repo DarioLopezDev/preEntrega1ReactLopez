57f6b915927d78da6b15acc378d8bbe679045ab6
Dario Lopez <dariolopezdeveloper@gmail.com>
2023-10-28 23:28:31 -0300

    reparo problema inicial

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-//Inicial
+/* //Inicial
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+) */
 
 //Para Usar ChakraUi
 import * as React from 'react'
